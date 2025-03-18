5b182a86f73b9ad8bdf5a73bebb75f1389c56eb7
La/in <141688294+Notlainn@users.noreply.github.com>
2025-03-18 15:27:58 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = "Hello, Wired!, My name is Lain!";
+        const speed = 150; // Velocidade da digitação
+        let index = 0;
+        let isDeleting = false;
+
+        function typeEffect() {
+            const element = document.getElementById("typing");
+
+            if (!isDeleting) {
+                element.textContent = text.slice(0, index++);
+                if (index > text.length) {
+                    isDeleting = true;
+                    setTimeout(typeEffect, 1000); // Pausa antes de apagar
+                    return;
+                }
+            } else {
+                element.textContent = text.slice(0, index--);
+                if (index < 0) {
+                    isDeleting = false;
+                    index = 0;
+                }
+            }
+
+            setTimeout(typeEffect, isDeleting ? speed / 2 : speed);
+        }
+
+        typeEffect();
